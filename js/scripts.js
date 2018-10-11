a73d94e2d93bd30687a817ad699bb1169d669bf3
India Mackinson <indjomac@gmail.com>
2018-10-10 23:17:28 -0400

    Trying to animate 2nd slide

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,29 @@ $(function (){
   console.log('DOM Loaded');
 
   $('.slick-slider').slick({
-
     });
 
+/* $('#slide1').on('beforeChange', function(event, slick, currentSlide, nextSlide){
+  console.log(nextSlide);
+  $('#car').animate({left: '600px'}, "slow");
+}); */
 
-      $('#car').animate({left: '600px'}, "slow");
 
+$('#slide1').ready(function(){
+  console.log('car animated');
+  $('#car').animate({left: '+=300px'}, 1000);
+});
+
+$('.slide1').click(function(beforeChange){
+  console.log(nextSlide);
+  $('#car').animate({left: '+=300px'}, 1000);
+  waitForAnimate: true;
+});
+
+$('#slide2').click(function(){
+  console.log('car animated 2');
+  $('#car-sensor').animate({left: '+=300px'}, 1000);
+});
 
 
 });
