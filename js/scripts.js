f210391e2de7ec977436baafccf333e53b2aad30
India Mackinson <indjomac@gmail.com>
2018-10-15 01:18:32 -0400

    Due to shortage of time, refining ideas to something more doable

--- js/scripts.js
+++ js/scripts.js
@@ -24,6 +24,21 @@ $(function (){
     console.log('car animated 2');
   });
 
+//Slide 2 -----------------------------------------------
+
+$('#car-sensor').on({
+  mouseenter: function(){
+    $('#lidar-graphic').css('display', 'block');
+    $('#radar-graphic').css('display', 'block');
+    $('#image-graphic').css('display', 'block');
+    $('#ultrasonic-graphic').css('display', 'block');
+  },
+  mouseleave: function(){
+  $(this).css('color', '#5b5c56');
+  $('#lidar-graphic').css('display', 'none');
+  $('#modal-ld').css('display', 'none');
+  },
+});
 
 
   $('#ld').on({
@@ -56,7 +71,6 @@ $(function (){
     mouseenter: function(){
       $(this).css('color', '#d388f2');
       $('#image-graphic').css('display', 'block');
-      $('#car-sensor').css('display', 'none');
       $('#modal-im').css('display', 'block');
 
     },
@@ -102,42 +116,16 @@ $(function (){
 }
 
 //Slide 3 -----------------------------------------------------
-// Original JavaScript code by Chirp Internet: www.chirp.com.au
-  // Please acknowledge use of this code by including this header.
 
-  window.requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
 
-    var field = document.getElementById("field");
-    var ball = document.getElementById("ball");
-    var ball2 = document.getElementById("ball2");
+    $('.tooltip').on({
+      mouseenter: function(){
+      $('#car-3').animate({left: '+=25%'}, 1000);
+      },
+      mouseleave: function(){
 
-    var maxX = field.clientWidth - ball.offsetWidth;
-    var maxY = field.clientHeight - ball.offsetHeight;
+      },
+    });
 
-    var duration = 10; // seconds
-    var gridSize = 50; // pixels
-
-    var start = null;
-
-    function step(timestamp)
-    {
-      var progress, x, y, y2;
-      if(start === null) start = timestamp;
-
-      progress = (timestamp - start) / duration / 1000; // percent
-
-      x = progress * maxX/gridSize; // x = ƒ(t)
-      y = 2 * Math.sin(x); // y = ƒ(x)
-      y2 = 1 * Math.cos(x);
-
-      ball.style.left = ball2.style.left = Math.min(maxX, gridSize * x) + "px";
-      ball.style.bottom = maxY/2 + (gridSize * y) + "px";
-      ball2.style.bottom = maxY/2 + (gridSize * y2) + "px";
-
-      if(progress >= 1) start = null; // reset to start position
-      requestAnimationFrame(step);
-    }
-
-    requestAnimationFrame(step);
 
 });
