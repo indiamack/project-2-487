a5b2650c64ef8607873664047542db262c2e5209
India Mackinson <indjomac@gmail.com>
2018-10-16 07:51:08 -0400

    Fiddling with image mapping, rethinking my design idea

--- js/scripts.js
+++ js/scripts.js
@@ -4,15 +4,17 @@ $(function (){
 
   $(document).ready(function() {
             $('.tooltip').tooltipster();
-            console.log('tooltip ready')
-        });
+            console.log('tooltip ready');
+
+          });
 
   $('.tooltip').tooltipster({
         theme: ['tooltipster-noir', 'tooltipster-noir-customized']
   });
 
   $('.slick-slider').slick({
-    });
+  });
+
 
   $('#slide1').ready(function(){
     console.log('car animated');
@@ -178,7 +180,136 @@ $('#car-sensor').on({
 
     // SLide 4------------------------------------------------
 
-$('img[usemap]').imageMap();
+    $.fn.rwdImageMaps = function() {
+
+                var $img = this;
+
+                var rwdImageMap = function() {
+                  $img.each(function() {
+                    if (typeof($(this).attr('usemap')) == 'undefined')
+                      return;
+
+                    var that = this,
+                      $that = $(that);
+
+                    // Since WebKit doesn't know the height until after the image has loaded, perform everything in an onload copy
+                    $('<img />').on('load', function() {
+                      var attrW = 'width',
+                        attrH = 'height',
+                        w = $that.attr(attrW),
+                        h = $that.attr(attrH);
+
+                      if (!w || !h) {
+                        var temp = new Image();
+                        temp.src = $that.attr('src');
+                        if (!w)
+                          w = temp.width;
+                        if (!h)
+                          h = temp.height;
+                      }
+
+                      var wPercent = $that.width()/100,
+                        hPercent = $that.height()/100,
+                        map = $that.attr('usemap').replace('#', ''),
+                        c = 'coords';
+
+                      $('map[name="' + map + '"]').find('area').each(function() {
+                        var $this = $(this);
+                        if (!$this.data(c))
+                          $this.data(c, $this.attr(c));
+
+                        var coords = $this.data(c).split(','),
+                          coordsPercent = new Array(coords.length);
+
+                        for (var i = 0; i < coordsPercent.length; ++i) {
+                          if (i % 2 === 0)
+                            coordsPercent[i] = parseInt(((coords[i]/w)*100)*wPercent);
+                          else
+                            coordsPercent[i] = parseInt(((coords[i]/h)*100)*hPercent);
+                        }
+                        $this.attr(c, coordsPercent.toString());
+                      });
+
+                    }).attr('src', $that.attr('src'));
+                  });
+                };
+
+                $(window).resize(rwdImageMaps).trigger('resize');
+
+                return this;
+
+                };
+
+    $('.slick-slide').ready(function(){
+      $("map[name=ped]").mapoid(function(){
+        $.fn.rwdImageMaps = function() {
+
+                    var $img = this;
+
+                    var rwdImageMap = function() {
+                      $img.each(function() {
+                        if (typeof($(this).attr('usemap')) == 'undefined')
+                          return;
+
+                        var that = this,
+                          $that = $(that);
+
+                        // Since WebKit doesn't know the height until after the image has loaded, perform everything in an onload copy
+                        $('<img />').on('load', function() {
+                          var attrW = 'width',
+                            attrH = 'height',
+                            w = $that.attr(attrW),
+                            h = $that.attr(attrH);
+
+                          if (!w || !h) {
+                            var temp = new Image();
+                            temp.src = $that.attr('src');
+                            if (!w)
+                              w = temp.width;
+                            if (!h)
+                              h = temp.height;
+                          }
+
+                          var wPercent = $that.width()/100,
+                            hPercent = $that.height()/100,
+                            map = $that.attr('usemap').replace('#', ''),
+                            c = 'coords';
+
+                          $('map[name="' + map + '"]').find('area').each(function() {
+                            var $this = $(this);
+                            if (!$this.data(c))
+                              $this.data(c, $this.attr(c));
+
+                            var coords = $this.data(c).split(','),
+                              coordsPercent = new Array(coords.length);
+
+                            for (var i = 0; i < coordsPercent.length; ++i) {
+                              if (i % 2 === 0)
+                                coordsPercent[i] = parseInt(((coords[i]/w)*100)*wPercent);
+                              else
+                                coordsPercent[i] = parseInt(((coords[i]/h)*100)*hPercent);
+                            }
+                            $this.attr(c, coordsPercent.toString());
+                          });
+
+                        }).attr('src', $that.attr('src'));
+                      });
+                    };
+
+                    $(window).resize(rwdImageMaps).trigger('resize');
+
+                    return this;
+
+                    };
+        $('img[usemap=ped]').rwdImageMaps(function(){
+          console.log('good morning');
+        });
+      });
+    });
+
+
+
+
 
 
 
